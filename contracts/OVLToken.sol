c69f87a0bc9b85f7e4951b7a00c17d7b2714d977
mikeyrf <mikeyrf@users.noreply.github.com>
2021-04-19 19:05:12 -0400

    init contracts

---
+++ contracts/OVLToken.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import ERC20 from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import AccessControl from "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract OVLToken is AccessControl, ERC20("Overlay", "OVL") {
+
+  bytes32 public constant ADMIN_ROLE = keccak256("ADMIN");
+  bytes32 public constant MINTER_ROLE = keccak256("MINTER");
+  bytes32 public constant BURNER_ROLE = keccak256("BURNER");
+
+  constructor() {
+    _setupRole(ADMIN_ROLE, msg.sender);
+    _setupRole(MINTER_ROLE, msg.sender);
+    _setRoleAdmin(MINTER_ROLE, ADMIN_ROLE);
+    _setRoleAdmin(BURNER_ROLE, ADMIN_ROLE);
+    _setRoleAdmin(ADMIN_ROLE, ADMIN_ROLE);
+  }
+
+  modifier onlyMinter() {
+    require(hasRole(MINTER_ROLE, msg.sender), "only minter");
+    _;
+  }
+
+  modifier onlyBurner() {
+    require(hasRole(BURNER_ROLE, msg.sender), "only burner");
+    _;
+  }
+
+  function mint(address _recipient, uint256 _amount) external onlyMinter {
+      _mint(_recipient, _amount);
+  }
+
+  function burn(address _account, uint256 _amount) external onlyBurner {
+      _burn(_account, _amount);
+  }
+
+}
