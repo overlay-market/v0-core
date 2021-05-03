fe69c8e45640655f2a8b43d657793a4878129ed1
mikeyrf <mikeyrf@users.noreply.github.com>
2021-05-03 09:47:47 -0400

    change contract naming to market

--- contracts/OVLMirinFactory.sol
+++ contracts/OVLMirinFactory.sol
@@ -4,38 +4,68 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "./interfaces/IMirinFactory.sol";
-import "./OVLMirinPosition.sol";
+import "./OVLMirinMarket.sol";
 import "./OVLToken.sol";
 
 contract OVLMirinFactory is Ownable {
 
+    // ovl erc20 token
     address public immutable ovl;
+    // mirin pool factory
     address public immutable mirinFactory;
 
-    mapping(address => bool) public isPosition;
-    address[] public allPositions;
+    mapping(address => bool) public isMarket;
+    address[] public allMarkets;
 
     constructor(address _ovl, address _mirinFactory) {
         ovl = _ovl;
         mirinFactory = _mirinFactory;
     }
 
-    // deploys new position contract for given mirin pool address
-    function deploy(address pool, uint256 cap, uint256 k) external onlyOwner returns (OVLMirinPosition position) {
+    // deploys new market contract for given mirin pool address
+    function deploy(
+        address pool,
+        uint256 cap,
+        uint256 k
+    ) external onlyOwner returns (OVLMirinMarket marketContract) {
         require(IMirinFactory(mirinFactory).isPool(pool), "!MirinPool");
-        position = new OVLMirinPosition(mirinFactory, pool, cap, k);
+        marketContract = new OVLMirinMarket(
+            mirinFactory,
+            pool,
+            cap,
+            k
+        );
+
+        isMarket[address(marketContract)] = true;
+        allMarkets.push(address(marketContract));
 
         // Give position contract mint/burn priveleges for OVL token
-        OVLToken(ovl).grantRole(OVLToken(ovl).MINTER_ROLE(), address(position));
-        OVLToken(ovl).grantRole(OVLToken(ovl).BURNER_ROLE(), address(position));
-
-        isPosition[address(position)] = true;
-        allPositions.push(address(position));
+        OVLToken(ovl).grantRole(OVLToken(ovl).MINTER_ROLE(), address(marketContract));
+        OVLToken(ovl).grantRole(OVLToken(ovl).BURNER_ROLE(), address(marketContract));
     }
 
-    // disables an existing position contract for a mirin market
-    function disable(address position) external onlyOwner {
-        require(isPosition[address(position)], "!enabled");
-        isPosition[address(position)] = false;
+    // disables an existing market contract for a mirin market
+    function disable(address market) external onlyOwner {
+        require(isMarket[market], "!enabled");
+        isMarket[market] = false;
+
+        // Revoke mint/burn roles for the position
+        OVLToken(ovl).revokeRole(OVLToken(ovl).MINTER_ROLE(), market);
+        OVLToken(ovl).revokeRole(OVLToken(ovl).BURNER_ROLE(), market);
+    }
+
+    // enables an existing market contract for a mirin market
+    function enable(address market) external onlyOwner {
+        require(!isMarket[market], "!disabled");
+        isMarket[market] = true;
+
+        // Give position contract mint/burn priveleges for OVL token
+        OVLToken(ovl).grantRole(OVLToken(ovl).MINTER_ROLE(), market);
+        OVLToken(ovl).grantRole(OVLToken(ovl).BURNER_ROLE(), market);
+    }
+
+    // TODO: adjust(k, cap, leverages) to allow gov to adjust per market params
+    function adjust(address market, uint256 cap, uint256 k) external onlyOwner {
+        OVLMirinMarket(market).adjust(cap, k);
     }
 }
--- contracts/OVLMirinMarket.sol
+++ contracts/OVLMirinMarket.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
-contract OVLMirinPosition is ERC1155("https://metadata.overlay.exchange/mirin/{id}.json") {
+contract OVLMirinMarket is ERC1155("https://metadata.overlay.exchange/mirin/{id}.json") {
 
     // OVLMirinFactory address
     address public immutable factory;
@@ -19,6 +19,11 @@ contract OVLMirinPosition is ERC1155("https://metadata.overlay.exchange/mirin/{i
 
     // TODO: OI long, OI short, ...
 
+    modifier onlyFactory {
+        require(msg.sender == factory, "!factory");
+        _;
+    }
+
     constructor(
         address _mirinFactory,
         address _mirinPool,
@@ -28,6 +33,12 @@ contract OVLMirinPosition is ERC1155("https://metadata.overlay.exchange/mirin/{i
         factory = msg.sender;
         mirinFactory = _mirinFactory;
         mirinPool = _mirinPool;
+        cap = _cap;
+        k = _k;
+    }
+
+    // adjusts params associated with this market
+    function adjust(uint256 _cap, uint256 _k) external onlyFactory {
         cap = _cap;
         k = _k;
     }
