cc011e34d271a07431daf2b9633afececdef33b8
mikeyrf <mikeyrf@users.noreply.github.com>
2021-05-02 22:15:41 -0400

    add initial factory deploy

--- contracts/OVLMirinFactory.sol
+++ contracts/OVLMirinFactory.sol
@@ -1,2 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+import "./interfaces/IMirinFactory.sol";
+import "./OVLMirinPosition.sol";
+import "./OVLToken.sol";
+
+contract OVLMirinFactory is Ownable {
+
+    address public immutable ovl;
+    address public immutable mirinFactory;
+
+    constructor(address _ovl, address _mirinFactory) {
+        ovl = _ovl;
+        mirinFactory = _mirinFactory;
+    }
+
+    // deploys new position contract for given mirin pool address
+    function deploy(address pool) external onlyOwner returns (OVLMirinPosition position) {
+        require(IMirinFactory(mirinFactory).isPool(pool), "!MirinPool");
+        position = new OVLMirinPosition(mirinFactory, pool);
+
+        // Give pool contract mint/burn priveleges for OVL token
+        OVLToken(ovl).grantRole(OVLToken(ovl).MINTER_ROLE(), address(position));
+        OVLToken(ovl).grantRole(OVLToken(ovl).BURNER_ROLE(), address(position));
+    }
+}
--- contracts/OVLMirinPosition.sol
+++ contracts/OVLMirinPosition.sol
@@ -1,5 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-/// Potentially have two functions for locking/unlocking: lockLong and lockShort && unlockLong and unlockShort
-/// ... so less complexity within function for if else statements
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract OVLMirinPosition is ERC1155("https://metadata.overlay.exchange/mirin/{id}.json") {
+
+    // OVLMirinFactory address
+    address public immutable factory;
+
+    // mirin pool and factory addresses
+    address public immutable mirinFactory;
+    address public immutable mirinPool;
+
+    constructor(
+        address _mirinFactory,
+        address _mirinPool
+    ) {
+        factory = msg.sender;
+        mirinFactory = _mirinFactory;
+        mirinPool = _mirinPool;
+    }
+}
