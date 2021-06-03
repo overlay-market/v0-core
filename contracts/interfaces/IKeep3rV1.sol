1e4d9130511d19ee7c8de60f7e38deb4ed3b4253
mikeyrf <mikeyrf@users.noreply.github.com>
2021-06-03 09:55:57 -0400

    rm periphery interfaces

--- contracts/interfaces/IKeep3rV1.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
-
-interface IKeep3rV1 {
-    function isKeeper(address) external returns (bool);
-    function worked(address keeper) external;
-}
