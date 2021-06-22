eace67417772b8a677aefb92890f6cfd82b669a5
realisation <realisation@protonmail.com>
2021-06-22 14:21:38 -0400

    added preceding and proceeding underscores for param/return values in interface to avoid noise of duplicate name error

--- contracts/interfaces/IOverlayV1Factory.sol
+++ contracts/interfaces/IOverlayV1Factory.sol
@@ -10,36 +10,36 @@ interface IOverlayV1Factory {
     );
 
     function getGlobalParams() external view returns (
-        uint16 fee, 
-        uint16 feeBurnRate, 
-        uint16 feeUpdateRewardsRate, 
-        address feeTo, 
-        uint8 marginMaintenance, 
-        uint8 marginBurnRate, 
-        address marginTo
+        uint16 fee_, 
+        uint16 feeBurnRate_, 
+        uint16 feeUpdateRewardsRate_, 
+        address feeTo_, 
+        uint8 marginMaintenance_, 
+        uint8 marginBurnRate_, 
+        address marginTo_
     );
 
     function getFeeParams() external view returns (
-        uint16 fee, 
-        uint16 feeBurnRate, 
-        uint16 feeUpdateRewardsRate, 
-        address feeTo
+        uint16 fee_, 
+        uint16 feeBurnRate_, 
+        uint16 feeUpdateRewardsRate_, 
+        address feeTo_
     );
 
     function getMarginParams() external view returns (
-        uint8 marginMaintenance, 
-        uint8 marginBurnRate, 
-        address marginTo
+        uint8 marginMaintenance_, 
+        uint8 marginBurnRate_, 
+        address marginTo_
     );
 
     function fee() external view returns (uint256);
 
     function updateMarket(
-        address market, 
-        address rewardsTo
+        address _market, 
+        address _rewardsTo
     ) external;
 
     function massUpdateMarkets(
-        address rewardsTo
+        address _rewardsTo
     ) external;
 }
