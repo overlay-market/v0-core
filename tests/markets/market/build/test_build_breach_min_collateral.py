5b5f28bb706860838831df320d1176f993dc62a5
realisation <realisation@protonmail.com>
2021-06-26 11:56:38 -0400

    build tests and gitignore

---
+++ tests/markets/market/build/test_build_breach_min_collateral.py
@@ -0,0 +1,8 @@
+import pytest
+
+from brownie import reverts
+from brownie.test import given, strategy
+from collections import OrderedDict
+
+def test_build_breach_min_collateral(token, market, bob):
+    pass
