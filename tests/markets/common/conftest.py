60346c45b49b68618fdc753af91cfd1a10953258
mikeyrf <mikeyrf@users.noreply.github.com>
2021-06-26 20:45:41 -0400

    fix update test

--- tests/markets/common/conftest.py
+++ tests/markets/common/conftest.py
@@ -82,7 +82,7 @@ def price_points_after(token):
     scope="module",
     params=[
         ("OverlayV1MirinFactory", [15, 5000, 100, ETH_ADDRESS, 60, 50, ETH_ADDRESS],
-         "OverlayV1MirinMarket", [True, 4, 24, 100, 100, OI_CAP*10**TOKEN_DECIMALS, 1, 8, AMOUNT_IN*10**TOKEN_DECIMALS],
+         "OverlayV1MirinMarket", [True, 4, 24, 100, 100, OI_CAP*10**TOKEN_DECIMALS, 3293944666953, 9007199254740992, AMOUNT_IN*10**TOKEN_DECIMALS],
          "MirinFactoryMock", [],
          "IMirinOracle"),
     ])
