5cffff66ed0ffce0e19d50b4210d93601bbf540b
realisation <realisation@protonmail.com>
2021-07-17 02:42:52 -0400

    added standard erc20 metadata views to ovl token interface

--- contracts/interfaces/IOverlayToken.sol
+++ contracts/interfaces/IOverlayToken.sol
@@ -17,6 +17,12 @@ interface IOverlayToken {
 
     function approve(address spender, uint256 amount) external returns (bool);
 
+    function decimals() external view returns (uint);
+
+    function symbol () external view returns (string memory);
+    
+    function name () external view returns (string memory);
+
     function transferFrom(
         address sender,
         address recipient,
