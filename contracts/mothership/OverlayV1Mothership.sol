0f8609efe9ded163682aa3cb7ce9ab303789d467
realisation <realisation@protonmail.com>
2021-09-20 20:38:39 -0400

    access control enumerable added to mothership

--- contracts/mothership/OverlayV1Mothership.sol
+++ contracts/mothership/OverlayV1Mothership.sol
@@ -1,13 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
-
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "../interfaces/IOverlayV1Market.sol";
 import "../OverlayToken.sol";
 
-contract OverlayV1Mothership is Ownable {
+contract OverlayV1Mothership is AccessControlEnumerable {
 
     uint16 public constant MIN_FEE = 1; // 0.01%
     uint16 public constant MAX_FEE = 100; // 1.00%
@@ -15,7 +13,10 @@ contract OverlayV1Mothership is Ownable {
     uint16 public constant MIN_MARGIN_MAINTENANCE = 100; // 1% maintenance
     uint16 public constant MAX_MARGIN_MAINTENANCE = 6000; // 60% maintenance
 
-    uint16 public constant RESOLUTION = 10**4; // bps
+    bytes32 public constant GOVERNOR = keccak256("GOVERNOR");
+    bytes32 public constant GUARDIAN = keccak256("GUARDIAN");
+    bytes32 public constant MINTER = keccak256("MINTER");
+    bytes32 public constant BURNER = keccak256("BURNER");
 
     // ovl erc20 token
     address public immutable ovl;
@@ -37,10 +38,23 @@ contract OverlayV1Mothership is Ownable {
     uint16 public marginBurnRate;
 
     // whether is a market AND is enabled
-    mapping(address => bool) public isMarket;
+    mapping(address => bool) public marketActive;
     // whether is an already created market: for easy access instead of looping through allMarkets
     mapping(address => bool) public marketExists;
+
+    mapping(address => bool) public collateralExists;
+    mapping(address => bool) public collateralActive;
     address[] public allMarkets;
+
+    modifier onlyGovernor () {
+        require(hasRole(GOVERNOR, msg.sender), "OVLV1:!gov");
+        _;
+    }
+
+    modifier onlyGuardian () {
+        require(hasRole(GUARDIAN, msg.sender), "OVLV1:!guard");
+        _;
+    }
 
     constructor(
         address _ovl,
@@ -71,56 +85,39 @@ contract OverlayV1Mothership is Ownable {
 
     /// @notice Initializes an existing market contract after deployment
     /// @dev Should be called after contract deployment in specific market factory.createMarket
-    function initializeMarket(address market) internal {
+    function initializeMarket(address market) internal onlyGovernor {
         marketExists[market] = true;
-        isMarket[market] = true;
+        marketActive[market] = true;
         allMarkets.push(market);
-
-        // Give market contract mint/burn priveleges for OVL
-        OverlayToken(ovl).grantRole(OverlayToken(ovl).MINTER_ROLE(), market);
-        OverlayToken(ovl).grantRole(OverlayToken(ovl).BURNER_ROLE(), market);
     }
 
     /// @notice Disables an existing market contract for a mirin market
-    function disableMarket(address market) external onlyOwner {
-        require(isMarket[market], "OVLV1: !enabled");
-        isMarket[market] = false;
-
-        // Revoke mint/burn roles for the market
-        OverlayToken(ovl).revokeRole(OverlayToken(ovl).MINTER_ROLE(), market);
-        OverlayToken(ovl).revokeRole(OverlayToken(ovl).BURNER_ROLE(), market);
+    function disableMarket(address market) external onlyGovernor {
+        require(marketActive[market], "OVLV1: !enabled");
+        marketActive[market] = false;
     }
 
     /// @notice Enables an existing market contract for a mirin market
-    function enableMarket(address market) external onlyOwner {
+    function enableMarket(address market) external onlyGovernor {
         require(marketExists[market], "OVLV1: !exists");
-        require(!isMarket[market], "OVLV1: !disabled");
-        isMarket[market] = true;
+        require(!marketActive[market], "OVLV1: !disabled");
+        marketActive[market] = true;
+    }
 
-        // Give market contract mint/burn priveleges for OVL token
-        OverlayToken(ovl).grantRole(OverlayToken(ovl).MINTER_ROLE(), market);
-        OverlayToken(ovl).grantRole(OverlayToken(ovl).BURNER_ROLE(), market);
+    function enableCollateral (address _collateral) external onlyGovernor {
+        require(collateralExists[_collateral], "OVLV1:!exists");
+        require(!collateralActive[_collateral], "OVLV1:!disabled");
+        OverlayToken(ovl).grantRole(OverlayToken(ovl).MINTER_ROLE(), _collateral);
+        OverlayToken(ovl).grantRole(OverlayToken(ovl).BURNER_ROLE(), _collateral);
+    }
+
+    function disableCollateral (address _collateral) external onlyGovernor {
+        require(collateralActive[_collateral], "OVLV1:!enabled");
+        OverlayToken(ovl).revokeRole(OverlayToken(ovl).MINTER_ROLE(), _collateral);
+        OverlayToken(ovl).revokeRole(OverlayToken(ovl).BURNER_ROLE(), _collateral);
     }
 
     /// @notice Allows gov to adjust per market params
-    function adjustPerMarketParams(
-        address market,
-        uint256 updatePeriod,
-        uint256 compoundingPeriod,
-        uint144 oiCap,
-        uint112 fundingKNumerator,
-        uint112 fundingKDenominator,
-        uint8 leverageMax
-    ) external onlyOwner {
-        IOverlayV1Market(market).adjustParams(
-            updatePeriod,
-            compoundingPeriod,
-            oiCap,
-            fundingKNumerator,
-            fundingKDenominator,
-            leverageMax
-        );
-    }
 
     /// @notice Allows gov to adjust global params
     function adjustGlobalParams(
@@ -131,7 +128,7 @@ contract OverlayV1Mothership is Ownable {
         uint16 _marginMaintenance,
         uint16 _marginBurnRate,
         uint16 _marginRewardRate
-    ) external onlyOwner {
+    ) external onlyGovernor {
         fee = _fee;
         feeBurnRate = _feeBurnRate;
         feeUpdateRewardsRate = _feeUpdateRewardsRate;
