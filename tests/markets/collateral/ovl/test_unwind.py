61a96c490f232a084befb0c40d658843e340ae62
realisation <realisation@protonmail.com>
2021-09-26 14:41:32 -0400

    some unwind tests

--- tests/markets/collateral/ovl/test_unwind.py
+++ tests/markets/collateral/ovl/test_unwind.py
@@ -47,11 +47,13 @@ def test_unwind_oi_removed(
 
     # Position info
     pid = tx_build.events['Build']['positionId']
-    (_, _, _, _, oi_shares_build, debt_build, cost_build, _) =\
+    (_, _, _, price_point, oi_shares_build, debt_build, cost_build, p_compounding) =\
         ovl_collateral.positions(pid)
 
+    chain.mine(timedelta=market.updatePeriod()+1)
+
     # Unwind
-    ovl_collateral.unwind(
+    tx = ovl_collateral.unwind(
         pid,
         oi_shares_build,
         {"from": bob}
@@ -64,43 +66,41 @@ def test_unwind_oi_removed(
     assert oi_shares_unwind == 0
 
 
-# # warning, dependent on what the price/mocks do
-# def test_unwind_revert_position_was_liquidated(
-#         ovl_collateral,
-#         mothership,
-#         market,
-#         collateral,
-#         token,
-#         bob,
-#         alice):
+# warning, dependent on what the price/mocks do
+def test_unwind_revert_position_was_liquidated(
+        ovl_collateral,
+        mothership,
+        market,
+        collateral,
+        token,
+        bob,
+        alice):
 
-#     collateral = 2e18
-#     leverage = 1
-#     is_long = True
+    collateral = 2e18
+    leverage = 1
+    is_long = True
 
-#     token.approve(ovl_collateral, collateral, {"from": bob})
-#     tx_build = ovl_collateral.build(
-#         market,
-#         collateral,
-#         leverage,
-#         is_long,
-#         {"from": bob}
-#     )
+    # token.approve(ovl_collateral, collateral, {"from": bob})
+    # tx_build = ovl_collateral.build(
+    #     market,
+    #     collateral,
+    #     leverage,
+    #     is_long,
+    #     {"from": bob}
+    # )
 
+    # with brownie.reverts("OVLV1:!shares"):
+    #     ovl_collateral.unwind(
+    #         1,
+    #         1e18,
+    #         { "from": bob }
+    #     );
 
+    # build a position
+    # liquidate a position
+    # try to unwind it and get a revert
 
-#     with brownie.reverts("OVLV1:!shares"):
-#         ovl_collateral.unwind(
-#             1,
-#             1e18,
-#             { "from": bob }
-#         );
-
-#     # build a position
-#     # liquidate a position
-#     # try to unwind it and get a revert
-
-#     pass
+    pass
 
 
 def test_unwind_from_queued_oi (ovl_collateral, bob):
@@ -112,43 +112,43 @@ def test_unwind_from_queued_oi (ovl_collateral, bob):
     pass
 
 
-# def test_that_comptroller_recorded_mint_or_burn (
-#     ovl_collateral, 
-#     mothership,
-#     token, 
-#     market, 
-#     bob
-# ):
+def test_that_comptroller_recorded_mint_or_burn (
+    ovl_collateral, 
+    mothership,
+    token, 
+    market, 
+    bob
+):
 
-#     update_period = market.updatePeriod()
+    update_period = market.updatePeriod()
 
-#     # when we unwind, seeing if there was a mint/burn, 
-#     # and see if the brrrrd variable has recorded it
-#     tx = ovl_collateral.build(
-#         market,
-#         1e18,
-#         1,
-#         True,
-#         { 'from': bob }
-#     )
+    # when we unwind, seeing if there was a mint/burn, 
+    # and see if the brrrrd variable has recorded it
+    tx = ovl_collateral.build(
+        market,
+        1e18,
+        1,
+        True,
+        { 'from': bob }
+    )
 
-#     pos_id = tx.events['Build']['positionId']
-#     bobs_shares = tx.events['Build']['oi']
+    pos_id = tx.events['Build']['positionId']
+    bobs_shares = tx.events['Build']['oi']
 
-#     chain.mine(timedelta=update_period*2)
+    chain.mine(timedelta=update_period*2)
 
-#     tx = ovl_collateral.unwind(
-#         pos_id,
-#         bobs_shares, 
-#         { "from": bob }
-#     )
+    tx = ovl_collateral.unwind(
+        pos_id,
+        bobs_shares, 
+        { "from": bob }
+    )
 
-#     fee = mothership.fee()
+    fee = mothership.fee()
 
-#     expected_brrrr = -(Decimal(1) - Decimal(1) * ( Decimal(fee) / Decimal(1e18) ))
-#     brrrrd = Decimal(market.brrrrd()) / Decimal(1e18)
+    expected_brrrr = -(Decimal(1) - Decimal(1) * ( Decimal(fee) / Decimal(1e18) ))
+    brrrrd = Decimal(market.brrrrd()) / Decimal(1e18)
 
-#     assert expected_brrrr == brrrrd
+    assert expected_brrrr == brrrrd
 
 
 
