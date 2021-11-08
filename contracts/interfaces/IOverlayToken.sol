801de00f04ae69374260f83e3f1cdb87827945ae
realisation <realisation@protonmail.com>
2021-11-08 13:17:55 -0500

    Rerouted paths to compose dependencies from /utils throughout app with new Overlay token in /ovl. deleted old root level OverlayToken.sol

--- contracts/interfaces/IOverlayToken.sol
+++ contracts/interfaces/IOverlayToken.sol
@@ -1,21 +1,12 @@
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
 interface IOverlayToken {
 
-    function burn(address account, uint256 amount) external;
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
-    function mint(address account, uint256 amount) external;
-
-    function totalSupply() external view returns (uint256);
-
-    function balanceOf(address account) external view returns (uint256);
-
-    function transfer(address recipient, uint256 amount) external returns (bool);
-
-    function allowance(address owner, address spender) external view returns (uint256);
-
-    function approve(address spender, uint256 amount) external returns (bool);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
     function decimals() external view returns (uint);
 
@@ -23,13 +14,85 @@ interface IOverlayToken {
     
     function name () external view returns (string memory);
 
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(
+        address account
+    ) external view returns (
+        uint256
+    );
+
+    function allowance(
+        address owner, 
+        address spender
+    ) external view returns (
+        uint256
+    );
+
+    function approve(
+        address spender, 
+        uint256 amount
+    ) external returns (
+        bool
+    );
+
+    function transfer(
+        address recipient, 
+        uint256 amount
+    ) external returns (
+        bool
+    );
+
     function transferFrom(
         address sender,
         address recipient,
         uint256 amount
-    ) external returns (bool);
+    ) external returns (
+        bool
+    );
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
+    function burn(
+        address account, 
+        uint256 amount
+    ) external;
 
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+    function mint(
+        address account, 
+        uint256 amount
+    ) external;
+
+    function transferBurn(
+        address recipient,
+        uint256 amount,
+        uint256 burnt
+    ) external returns (
+        bool
+    );
+
+    function transferFromBurn(
+        address sender,
+        address recipient,
+        uint256 amount,
+        uint256 burnt
+    ) external returns (
+        bool
+    );
+
+    function transferMint(
+        address recipient,
+        uint256 amount,
+        uint256 mint
+    ) external returns (
+        bool
+    );
+
+    function transferFromMint(
+        address sender,
+        address recipient,
+        uint256 amount,
+        uint256 mint
+    ) external returns (
+        bool
+    );
+
 }
