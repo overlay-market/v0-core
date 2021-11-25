4cda4b19855e1777f7c74e992fc60e67884f6a1e
realisation <realisation@protonmail.com>
2021-11-25 00:03:22 -0500

    comitting progress on shim

--- contracts/shims/BalancerQueries.sol
+++ contracts/shims/BalancerQueries.sol
@@ -23,6 +23,21 @@ contract BalancerQueries {
 
     }
 
+        int256 private constant _LOG_COMPRESSION_FACTOR = 1e14;
+    int256 private constant _HALF_LOG_COMPRESSION_FACTOR = 0.5e14;
+
+    function toLowResLog(uint256 value) internal pure returns (int256) {
+        int256 ln = LogExpMath.ln(int256(value));
+
+        // Rounding division for signed numerator
+        int256 lnWithError = (ln > 0 ? ln + _HALF_LOG_COMPRESSION_FACTOR : ln - _HALF_LOG_COMPRESSION_FACTOR);
+        return lnWithError / _LOG_COMPRESSION_FACTOR;
+    }
+
+    function fromLowResLog(int256 value) internal pure returns (uint256) {
+        return uint256(LogExpMath.exp(value * _LOG_COMPRESSION_FACTOR));
+    }
+
     function twa () public view returns (
         uint ten_,
         uint hour_,
@@ -107,7 +122,6 @@ contract BalancerQueries {
             .mulDown(_w0.divUp(_w1))
             .powUp(_w1)
             .mulUp(_tenInv);
-
 
     }
 
